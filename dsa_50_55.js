b413946059df503123be545388f928d9b35583eb
Imtiaz Shawn <101446861+imtiazshawn@users.noreply.github.com>
2026-02-11 17:49:06 +0600

    Add reversal algorithm for rotating an array
    Implement reversal algorithm for array rotation.

--- dsa_50_55.js
+++ dsa_50_55.js
@@ -43,3 +43,23 @@ for (let i = 0; i < arr.length; i++) {
 arr[arr.length - 1] = copy;
 
 console.log(arr);
+
+// Reversal Algorithm for Array Rotation
+let arr = [1, 2, 3, 4, 5];
+
+let k = 3;
+k = k % arr.length;
+
+reverse(0, k - 1);
+reverse(k, arr.length - 1);
+reverse(0, arr.length - 1);
+
+function reverse(i, j) {
+    while (i < j) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+}
